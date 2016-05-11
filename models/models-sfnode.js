03f544894435981d14e7455f4c909f5f87d646e3
Trewaters <trewaters@hotmail.com>
2016-05-11 05:04:32 -0700

    [ADD] morgan logging. Started on Mongoose schema

---
+++ models/models-sfnode.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+    
+var googleEvent = new Schema({
+    // [TO DO] I need to know what data will be used so I can add it to the schema
+});
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 var express = require('express'); // express
-var app = express(); // express
+var app_express = express(); // express
 
-app.get('/', function(req,res){
+var mongoose = require('mongoose'); // mongoose
+
+// [NOTE] use fs for writing logs to file
+// var fs = require('fs'); // morgan
+var logger = require('morgan'); // morgan
+
+// [NOTE] logging format that works well for development
+app_express.use(logger('dev'));  // morgan
+
+/*
+// [NOTE] Standard log output
+app_express.use('common');  // morgan
+// [NOTE] Minimal output
+app_express.use('tiny');  // morgan
+
+// [NOTE] Write logs to file
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'}); // morgan
+app_express.use(morgan('common', {stream: accessLogStream})); // morgan
+*/
+
+mongoose.connect('mongodb://localhost/sfnode2016'); // mongoose
+
+app_express.get('/', function(req,res){
     res.send(222); // [NOTE] show this response with Morgan logger, then change to ( res.send(222); )
 }); // express
 
-var server = app.listen({
+var server = app_express.listen({
     host: 'localhost',
     port: 3000
 }, function () {
